754f16f8dd38c9394862d8e03658f51b9fc7a65c
eric_freitas <eric_freitas@hotmail.com>
2015-03-05 23:00:51 -0300

    versão funcional refatorada sem palavra

--- src/hangman/main.clj
+++ src/hangman/main.clj
@@ -40,30 +40,20 @@
      |
     _|___")
 
-;TODO:
-(def hits (java.util.HashSet.))
-
-(defn err-handler [mistakes]
+(defn- err-handler [mistakes]
   (case mistakes
     4 (javax.swing.JOptionPane/showMessageDialog nil head)
     3 (javax.swing.JOptionPane/showMessageDialog nil body)
     2 (javax.swing.JOptionPane/showMessageDialog nil arms)
     1 (javax.swing.JOptionPane/showMessageDialog nil legs)))
 
-(defn end-game []
+(defn- end-game []
   (javax.swing.JOptionPane/showMessageDialog nil "Você GANHOU!")
   (System/exit 0))
 
-(defn success-handler [c chs]
-  (.add hits c)
+(defn- success-handler [c chs hits]
   (javax.swing.JOptionPane/showMessageDialog nil "Você acertou!")
-  (if (= (.size hits) (count chs)) (end-game)))
-
-;(defn write-riddle []
-;  (loop [i 0]
-;    (when(< i (.length word))
-;      (println (java.lang.Boolean/valueOf (.contains hits (.charAt word i))))
-;      (recur (+ i 1)))))
+  (if (= (count (into hits c)) (count chs)) (end-game)))
 
 (defn- obtem-set-chs [word]
   (into #{} (seq (.toCharArray word))))
@@ -71,9 +61,11 @@
 (defn start-game []
   (let [word (javax.swing.JOptionPane/showInputDialog "Informe a palavra chave")
         chs (obtem-set-chs word)]
-    (loop [mistakes 4]
+    (loop [mistakes 4 
+           hits #{}]
       (when (> mistakes 0)
         (let [c (javax.swing.JOptionPane/showInputDialog "Informe um caractere")] 
-          (if (.contains word c) (success-handler c chs) (err-handler mistakes))
-          (recur (if (.contains word c) mistakes (dec mistakes))))))
+          (if (.contains word c) (success-handler c chs hits) (err-handler mistakes))
+          (recur (if (.contains word c) mistakes (dec mistakes))
+                 (into hits c)))))
     (javax.swing.JOptionPane/showMessageDialog nil "Você perdeu")))
