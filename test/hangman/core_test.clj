527bf18162654bc9c459743096caa8682d85140a
eric_freitas <eric_freitas@hotmail.com>
2015-04-02 22:26:51 -0300

    removendo quebra de encapsulamento dos testes

--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -1,22 +1,18 @@
 (ns hangman.core-test
   (:require [midje.sweet :refer :all]
             [hangman.main :refer :all]))
-
-(fact "new game is not lost" 
-      (let [game (new-game "word")]
-        (lose? game) => false))  
-
-(fact "new game is not won" 
-      (let [game (new-game "word")]
-        (win? game) => false))
-
-(fact "attempt is right" 
-      (let [game (new-game "aligator")]
-        (with-attempt game "a") => {:word "aligator" :chs (into #{} (seq "aligator")) :hits #{\a } :mistakes 4 :message "a_ _ _ a_ _ _ "}))
-
-(fact "attempt is wrong"
-      (let [game (new-game "aligator")]
-        (with-attempt game "s") => {:word "aligator" :chs (into #{} (seq "aligator")) :hits #{} :mistakes 3 :message head}))
-
+(let [game (new-game "aligator")]
+  (fact "new game is not lost" 
+        (lose? game) => false)
+  
+  (fact "new game is not won" 
+        (win? game) => false)
+  
+  (fact "attempt is right" 
+        (message (with-attempt game "a")) => "a_ _ _ a_ _ _ ")
+  
+  (fact "attempt is wrong"
+        (message (with-attempt game "s")) => head)  
+)
 
 ;(do (require 'midje.repl) (midje.repl/autotest))
