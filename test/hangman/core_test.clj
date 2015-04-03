13e79fdbba906d557ccd11755057f33e0e855f7f
eric_freitas <eric_freitas@hotmail.com>
2015-04-02 22:10:45 -0300

    testes

--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -2,6 +2,16 @@
   (:require [midje.sweet :refer :all]
             [hangman.main :refer :all]))
 
-(fact "1 = 1" 
-      1 => 2)  
+(fact "new game is not lost" 
+      (let [game (new-game "word")]
+        (lose? game) => false))  
+
+(fact "new game is not won" 
+      (let [game (new-game "word")]
+        (win? game) => false))
+
+(fact "new game is not won" 
+      (let [game (new-game "word")]
+        (win? game) => false))
+
 ;(do (require 'midje.repl) (midje.repl/autotest))
