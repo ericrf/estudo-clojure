3a8dba6739d056ac0fe2848ada02e2d6beafa301
eric_freitas <eric_freitas@hotmail.com>
2015-04-06 20:55:23 -0300

    refatoração teste com letras corretas

--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -2,28 +2,20 @@
   (:require [midje.sweet :refer :all]
             [hangman.main :refer :all]))
 (let [game (new-game "aligator")]
-  (fact "new game is not lost" 
-        (lose? game) => false)
+ (fact "new game is not lost" 
+       (lose? game) => false)
   
-  (fact "new game is not won" 
-        (win? game) => false)
+ (fact "new game is not won" 
+       (win? game) => false)
   
-  (fact "attempt is right" 
-        (message (with-attempt game "a")) => "a_ _ _ a_ _ _ ")
+ (fact "attempt is right" 
+       (message (with-attempt game "a")) => "a_ _ _ a_ _ _ ")
   
-  (fact "attempt is wrong"
-        (message (with-attempt game "s")) => head)
-  
-  (fact "correct letters win the game"
-        (-> game 
-            (with-attempt "a")
-            (with-attempt "l")
-            (with-attempt "i")
-            (with-attempt "g")
-            (with-attempt "t")
-            (with-attempt "o")
-            (with-attempt "r")
-            win?) => true)
+ (fact "attempt is wrong"
+       (message (with-attempt game "s")) => head)
+
+ (fact "correct letters win the game" 
+       (win? (assoc game :hits (seq "aligtor"))) => true)
   
  (let [game' (-> game (with-attempt "w"))
        game'' (-> game' (with-attempt "s"))
