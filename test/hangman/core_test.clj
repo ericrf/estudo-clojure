6ec267b304b4aeccf74cc8a20ad5b63f12b31c34
Altieres Lopes <altiereslopes@webgoal.com.br>
2015-04-07 19:11:21 -0300

    refatoração testes usando reduce

--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -1,30 +1,38 @@
 (ns hangman.core-test
   (:require [midje.sweet :refer :all]
             [hangman.main :refer :all]))
-(let [game (new-game "aligator")]
- (fact "new game is not lost" 
-       (lose? game) => false)
-  
- (fact "new game is not won" 
-       (win? game) => false)
-  
- (fact "attempt is right" 
-       (message (with-attempt game "a")) => "a_ _ _ a_ _ _ ")
-  
- (fact "attempt is wrong"
-       (message (with-attempt game "s")) => head)
 
- (fact "correct letters win the game" 
-       (win? (assoc game :hits (seq "aligtor"))) => true)
+(let [game (new-game "aligator")]
+  (defn with-attempts [game, guesses]
+    (reduce (fn [game' guess] (with-attempt game' guess)) game guesses))
+
+  (fact "new game is not lost" 
+        (lose? game) => false)
   
- (let [game' (-> game (with-attempt "w"))
-       game'' (-> game' (with-attempt "s"))
-       game'''(-> game'' (with-attempt "p"))
-       game''''(-> game''' (with-attempt "x"))]
-   (fact "first error" (:message game') => head)
-   (fact "second error" (:message game'') => body)
-   (fact "third error" (:message game''') => arms)
-   (fact "last error" (:message game'''') => legs)
-   (fact "lose the game " (lose? game'''') => true)))
+  (fact "new game is not won" 
+        (win? game) => false)
+  
+  (fact "correct letters win the game" 
+        (win? (with-attempts game ["a" "l" "i" "g" "t" "o" "r"])) => true)
+ 
+  (fact "attempt is right" 
+        (message (with-attempts game ["a" "l" "r"])) => "al_ _ a_ _ r")
+  
+  (fact "first error"
+        (message (with-attempts game ["w"])) => head)
+ 
+  (fact "second error"
+        (message (with-attempts game ["w" "s"])) => body)
+ 
+  (fact "third error"
+        (message (with-attempts game ["w" "s" "p"])) => arms)
+ 
+  (fact "last error"
+        (message (with-attempts game ["w" "s" "p" "x"])) => legs)
+ 
+  (fact "lose the game"
+        (lose? (with-attempts game ["w" "s" "p" "x"])) => true)
+ 
+)
 
 ;(do (require 'midje.repl) (midje.repl/autotest))
