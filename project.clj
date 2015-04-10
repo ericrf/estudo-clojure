ecee45c39b9b4aa6093c08d57026f7c40648c491
Altieres Lopes <altiereslopes@webgoal.com.br>
2015-04-09 22:56:48 -0300

    testes async

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
 ;  :license {:name "Eclipse Public License"
 ;            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [midje "1.6.3"]])
 
---
+++ test/hangman/async_test.clj
@@ -0,0 +1,31 @@
+(ns hangman.async_test
+  (:require [midje.sweet :refer :all]
+            [clojure.core.async :as async :refer :all]))
+
+
+(require '[clojure.core.async :as async :refer :all])
+
+; Consome tudo que for colocado no canal
+(let [c (chan)]
+  (thread
+    (loop []
+      (println "altz: " (<!! c))
+      (recur)))
+  (dotimes [n 5]
+    (>!! c n)))
+
+
+; Consome tudo que foi colocado no canal e fica esperando mais...
+(let [c (chan)]
+  (thread
+    (loop [i 4]
+      (cond
+        (> i 0) (println "altz: " i ":" (<!! c)))
+        :else
+          (recur (dec i))))
+  (dotimes [n 5]
+    (>!! c n))
+  (println "fim!"))
+
+
+
